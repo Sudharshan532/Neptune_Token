1b77d3bad6b8a0728523211cb3c923f2fe36891e
Sudharshan Reddy Chennupalle <76432805+Rafael-32@users.noreply.github.com>
2022-08-15 00:21:28 +0530

    Update contract-6b92dbb94c.sol

--- contract-6b92dbb94c.sol
+++ contract-6b92dbb94c.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 
 // Here i am using "ERC20Burnable" to burn the Tokens
-
+// Using the mint option in Remix IDE we can be able to generate additional supply of tokens
 contract Neptune is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("Neptune", "NPT") {
         _mint(msg.sender, 100000 * 10 ** decimals());
