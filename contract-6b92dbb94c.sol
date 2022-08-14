b598769a748bdaeb580c0e6edbf54b2416047aca
Rafael-32 <76432805+Rafael-32@users.noreply.github.com>
2022-08-15 00:18:36 +0530

    Create contract-6b92dbb94c.sol

---
+++ contract-6b92dbb94c.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+
+// Here i am using "ERC20Burnable" to burn the Tokens
+
+contract Neptune is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Neptune", "NPT") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
